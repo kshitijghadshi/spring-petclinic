d3967d506acf7922c43315c6e9909ffc164fcc53
kshitijghadshi <kshitijghadshi@gmail.com>
2025-08-18 12:15:05 +0530

    Update main.tf
    Signed-off-by: kshitijghadshi <kshitijghadshi@gmail.com>

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,14 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.44.0, < 6.0.0"
+      version = "~> 5.47.0" # hard pin to last stable 5.x release
     }
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
